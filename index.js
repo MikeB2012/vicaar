8ed1dd8069e1a7b04d51b0e5d35b5ae4013fc9dd
rangermauve <RangerMauve@hotmail.com>
2017-10-08 15:00:18 -0400

    Added stuff

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+var brain = require("brain.js");
+var NeuralNetwork = brain.NeuralNetwork;
+
+function load(json) {
+	var net = new NeuralNetwork();
+	net.fromJSON(json);
+	return net;
+}
+
+function run(net, data) {
+	return net.run(data);
+}
+
+function train(net, data) {
+
+}
